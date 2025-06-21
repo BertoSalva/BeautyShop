8b384451b65d8bb57724cadb99429184b2a7b213
Mila <103040988+mila0523@users.noreply.github.com>
2025-06-21 18:38:35 +0200

    Update AutoCompleteClientSelect.jsx
    Implement test users select

--- src/components/common/AutoCompleteClientSelect.jsx
+++ src/components/common/AutoCompleteClientSelect.jsx
@@ -7,13 +7,10 @@ import { TextField } from "@mui/material";
 
 function AutoCompleteClientSelect() {
     const top100Films = [
-        { title: 'The Shawshank Redemption', year: 1994 },
-        { title: 'The Godfather', year: 1972 },
-        { title: 'The Godfather: Part II', year: 1974 },
-        { title: 'The Dark Knight', year: 2008 },
-        { title: '12 Angry Men', year: 1957 },
-        { title: "Schindler's List", year: 1993 },
-        { title: 'Pulp Fiction', year: 1994 }
+        { title: 'Test', year: 1994 },
+        { title: 'Users', year: 1972 },
+        { title: 'Only', year: 1974 },
+        { title: 'Mila', year: 2008 },
     ];
 
     const defaultProps = {
